48b10399e6c94a20532906875a3c8c2a31a27b16
Debmallya Bhattacharya <admin@Debmallyas-MacBook-Air.local>
2020-06-15 22:00:50 +0100

    added redux-logger for dev

--- src/store.js
+++ src/store.js
@@ -1,6 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
 import rootReducer from './rootReducer'
+
+const middleware = getDefaultMiddleware()
+if (process.env.NODE_ENV !== 'production') {
+  let logger = require('redux-logger')
+  middleware.push(logger)
+}
 
 export default configureStore({
   reducer: rootReducer,
+  middleware,
 })
