0f917509c1ff2d47a35f2c64f54b524fc9e0f34f
Debmallya Bhattacharya <admin@Debmallyas-MacBook-Air.local>
2020-06-16 15:52:15 +0100

    movieAPI

---
+++ src/services/movie/api.js
@@ -0,0 +1,28 @@
+import axios from '../axios'
+
+const fetchBySearch = async (searchTerm, page, type, year) => {
+  const { data } = await axios.get('/', {
+    params: {
+      s: searchTerm,
+      page,
+      type,
+      y: year,
+    },
+  })
+  return data
+}
+
+const fetchByID = async (id) => {
+  const { data } = await axios.get('/', {
+    params: {
+      i: id,
+      plot: 'full',
+    },
+  })
+  return data
+}
+
+export default {
+  fetchBySearch,
+  fetchByID,
+}
