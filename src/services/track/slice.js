6245f645425d6a419c12410806eeb724ea1f23c4
Debmallya Bhattacharya <admin@Debmallyas-MacBook-Air.local>
2020-06-16 09:52:13 +0100

    removed commented code

--- src/services/track/slice.js
+++ src/services/track/slice.js
@@ -12,15 +12,4 @@ export const slice = createSlice({
 
 export const { setTracks } = slice.actions
 
-// export const incrementAsync = (amount) => (dispatch) => {
-//   setTimeout(() => {
-//     dispatch(incrementByAmount(amount))
-//   }, 1000)
-// }
-
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state) => state.counter.value)`
-// export const selectCount = (state) => state.counter.value
-
 export default slice.reducer
