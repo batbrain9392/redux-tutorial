28178ef410b002f50491ca824101700561e7450d
Debmallya Bhattacharya <admin@Debmallyas-MacBook-Air.local>
2020-06-17 10:10:46 +0100

    router set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Movie from './scenes/Movie'
 import Movies from './scenes/Movies'
 
 function App() {
-  return <Movies />
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path='/:id' component={Movie} />
+        <Route path='/' exact component={Movies} />
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
 export default App
