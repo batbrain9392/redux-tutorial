76b98df2919abcae685811e5fb5c2ab458333284
Debmallya Bhattacharya <batbrain9392@gmail.com>
2020-06-17 20:57:18 +0100

    removed redux-logger; added @material-ui/lab

--- src/store.js
+++ src/store.js
@@ -1,17 +1,10 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
 import filter from './services/filter/slice'
 import movie from './services/movie/slice'
-
-const middleware = getDefaultMiddleware()
-if (process.env.NODE_ENV !== 'production') {
-  let logger = require('redux-logger').default
-  middleware.push(logger)
-}
 
 export default configureStore({
   reducer: {
     filter,
     movie,
   },
-  middleware,
 })
