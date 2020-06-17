73ea351a0854be0a07b1d3d754f425c6b13cc7bf
Debmallya Bhattacharya <admin@Debmallyas-MacBook-Air.local>
2020-06-17 09:02:23 +0100

    deleted test code

--- src/components/Stream/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Stream from './presenter'
-import { useSelector } from 'react-redux'
-
-export default () => {
-  const tracks = useSelector((state) => state.track)
-
-  return <Stream tracks={tracks} />
-}
