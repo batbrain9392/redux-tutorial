f4f37f71707edb7d053c4f676173a0cc6755f3fa
batbrain9392 <batbrain9392@gmail.com>
2020-06-19 10:39:27 +0000

    Deploying to gh-pages from  @ 585139602541b3ad2a704f3a8c1c30c3eaa89486 🚀

---
+++ precache-manifest.a5d35b896f6222b7fdf01f2c3f6511a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9de188ddc10c5c007b623f795dfbe9dd",
+    "url": "/redux-tutorial/index.html"
+  },
+  {
+    "revision": "a6d0e4cd618a3bd6cb65",
+    "url": "/redux-tutorial/static/css/main.d1699770.chunk.css"
+  },
+  {
+    "revision": "73983739135868bc0378",
+    "url": "/redux-tutorial/static/js/2.7c403822.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/redux-tutorial/static/js/2.7c403822.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6d0e4cd618a3bd6cb65",
+    "url": "/redux-tutorial/static/js/main.9bbcd446.chunk.js"
+  },
+  {
+    "revision": "6092f93710579cc4e6ab",
+    "url": "/redux-tutorial/static/js/runtime-main.ab553040.js"
+  }
+]);
