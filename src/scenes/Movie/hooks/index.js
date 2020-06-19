dbe1b488b519c39867dbfa1b4fc6f9c884e4cc5e
Debmallya Bhattacharya <batbrain9392@gmail.com>
2020-06-19 17:51:21 +0100

    cancellable axios request

--- src/scenes/Movie/hooks/index.js
+++ src/scenes/Movie/hooks/index.js
@@ -1,5 +1,6 @@
 import { useReducer, useEffect } from 'react'
 import moviAPI from '../../../services/movie/api'
+import axios, { getSource } from '../../../services/axios'
 
 export const reducer = (state, action) => {
   switch (action.type) {
@@ -35,15 +36,22 @@ export const useGetMovie = (id) => {
 
   useEffect(() => {
     dispatch({ type: 'start' })
+    const source = getSource()
     const fetchData = async () => {
       try {
-        const result = await moviAPI.fetchByID(id)
+        const result = await moviAPI.fetchByID(id, source.token)
         dispatch({ type: 'success', payload: result })
       } catch (error) {
-        dispatch({ type: 'error', payload: error.message })
+        if (!axios.isCancel(error)) {
+          dispatch({ type: 'error', payload: error.message })
+        }
       }
     }
     fetchData()
+
+    return () => {
+      source.cancel()
+    }
   }, [id])
 
   return state
