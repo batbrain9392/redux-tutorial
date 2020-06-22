5ed029954c934dff3af57803bcd7e3e66a781f5f
Debmallya Bhattacharya <batbrain9392@gmail.com>
2020-06-22 10:42:31 +0100

    minor fix

--- src/scenes/Movies/components/SearchForm/presenter.jsx
+++ src/scenes/Movies/components/SearchForm/presenter.jsx
@@ -6,6 +6,7 @@ import Divider from '@material-ui/core/Divider'
 import IconButton from '@material-ui/core/IconButton'
 import SearchIcon from '@material-ui/icons/Search'
 import ClearIcon from '@material-ui/icons/Clear'
+import FormHelperText from '@material-ui/core/FormHelperText'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -39,35 +40,38 @@ const SearchForm = ({
   const classes = useStyles()
 
   return (
-    <Paper
-      component='form'
-      className={classes.root}
-      onSubmit={onSubmit}
-      onReset={onReset}>
-      <InputBase
-        inputRef={inputRef}
-        className={classes.input}
-        placeholder='Search movies'
-        inputProps={{ 'aria-label': 'search movies' }}
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      />
-      {searchTerm && (
+    <>
+      <Paper
+        component='form'
+        className={classes.root}
+        onSubmit={onSubmit}
+        onReset={onReset}>
+        <InputBase
+          inputRef={inputRef}
+          className={classes.input}
+          placeholder='Search movies'
+          inputProps={{ 'aria-label': 'search movies' }}
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        {searchTerm && (
+          <IconButton
+            className={classes.iconButton}
+            aria-label='clear'
+            type='reset'>
+            <ClearIcon />
+          </IconButton>
+        )}
+        <Divider className={classes.divider} orientation='vertical' />
         <IconButton
           className={classes.iconButton}
-          aria-label='clear'
-          type='reset'>
-          <ClearIcon />
+          aria-label='search'
+          type='submit'>
+          <SearchIcon />
         </IconButton>
-      )}
-      <Divider className={classes.divider} orientation='vertical' />
-      <IconButton
-        className={classes.iconButton}
-        aria-label='search'
-        type='submit'>
-        <SearchIcon />
-      </IconButton>
-    </Paper>
+      </Paper>
+      {error && <FormHelperText error>{error}</FormHelperText>}
+    </>
   )
 }
 
